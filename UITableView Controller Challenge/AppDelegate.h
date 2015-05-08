59839ceb0c42a137c856a0330b82ec7889f4b424
Matthew Surowiec <mp5auc3@gmail.com>
2015-05-08 12:34:59 -0400

    Initial Commit

---
+++ UITableView Controller Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView Controller Challenge
+//
+//  Created by Matthew Surowiec on 5/8/15.
+//  Copyright (c) 2015 MPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
